290623343fbb025aed91d8544e152e9a8d75188e
Vinicius Zapalá dos Santos <40218275+ViniZap4@users.noreply.github.com>
2022-08-13 19:53:29 -0300

    add react-router-dom and styled-components

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import GlobalStyle from '../Styles/global';
+
 const App: React.FC = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1> Hello World! </h1>
     </div>
   );
